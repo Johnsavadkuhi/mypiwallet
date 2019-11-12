02700089f749bc6c04aa56661c44150f411ca5b0
mohammad savadkuhi <johnsavadkuhi@Johns-MacBook-Pro.local>
2019-11-12 22:08:16 +0330

    add file for request string

---
+++ src/request/index.js
@@ -0,0 +1,58 @@
+export const GET_BLOCK_NUMBER = {
+    query: `
+    query {
+        getBlockNumber
+    }
+    `
+}
+
+export const GET_PENDING_TRANSACTIONS = {
+    query: `
+    query{
+   getPendingTransactions
+   {
+    hash
+    nonce
+    blockHash
+    transactionIndex
+    from
+    to
+    value
+    gasPrice
+    gas
+    input
+        }
+    }`
+};
+
+export const GET_BLOCK = function(input){
+    return {
+     query: `
+    query {
+        getBlock(numberBlock:"${input}"){
+number
+hash
+parentHash
+nonce
+sha3Uncles
+logsBloom
+transactionsRoot
+stateRoot
+receiptsRoot
+miner
+difficulty
+totalDifficulty
+extraData
+size
+gasLimit
+gasUsed
+timestamp
+            
+        }
+    }           
+    `
+}
+}
+
+
+
