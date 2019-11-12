7c7f7c9147c39acc992ee75b2b75d9d942c20167
mohammad savadkuhi <johnsavadkuhi@Johns-MacBook-Pro.local>
2019-11-13 01:13:15 +0330

    add transactions components

---
+++ src/components/Transactions.js
@@ -0,0 +1,29 @@
+import React from 'react'; 
+import {GET_BLOCK , GET_BLOCK_NUMBER} from '../request/'; 
+import Mnoparma from './methods_with_no_param'; 
+import Moneparam from './methods_with_one_param';
+
+function Transactions (){
+
+    return (<>
+
+        <div className="container is-fluid">
+          <div className="columns has-text-grey	">
+            <div className="column is-half is-offset-one-quarter">
+
+              <Mnoparma header="getBlockNumber" request={GET_BLOCK_NUMBER} />
+
+              <Moneparam header="getBlock" request={GET_BLOCK} />
+
+            </div>
+
+
+          </div>
+        </div>
+      
+      
+    </>)
+
+}
+
+export default Transactions; 
