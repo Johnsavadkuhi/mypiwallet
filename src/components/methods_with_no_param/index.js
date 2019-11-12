2b8beb5c7e009804c010dce47ccb5c327400195d
mohammad savadkuhi <johnsavadkuhi@Johns-MacBook-Pro.local>
2019-11-13 01:13:43 +0330

    update

--- src/components/methods_with_no_param/index.js
+++ src/components/methods_with_no_param/index.js
@@ -2,10 +2,8 @@ import React, { useState } from 'react';
 
 function Mnoparam(props) {
 
-
     const [data, setData] = useState('');
     const [isExec, setIsExec] = useState(false);
-
 
     const handleClick = e => {
         e.preventDefault();
@@ -35,34 +33,28 @@ function Mnoparam(props) {
     return (
         <>
             <hr />
-            {isExec ? 
-            <div className="notification ">
-                <button onClick={handleClose} className="delete"></button>
-                <div>
-                <div className="card-content">
-                    <pre> {JSON.stringify(data, null, 2)}</pre>
-                </div>  
-                
-            </div>
-            </div>
-             :
-            <div className="card">
-                    <header className="card-header">
-                        <p className="card-header-title has-text-danger	">
-                            {props.header}
-                         </p>
+            {isExec ?
+                <div className="notification ">
+                    <button onClick={handleClose} className="delete"></button>
+                    <div>
+                        <div className="card-content">
+                            <pre> {JSON.stringify(data, null, 2)}</pre>
+                        </div>
 
-                        <a href="void" className="card-header-icon" aria-label="more options">
-                            <span className="icon">
-                                <i className="fas fa-angle-down" aria-hidden="true"></i>
-                            </span>
-                        </a>
+                    </div>
+                </div>
+                :
+                <div className="card">
+                    <header className="card-header">
+                        <code className="card-header-title has-text-danger is-size-5 is-size-6-mobile">
+                            {props.header}
+                        </code>
                     </header>
                     <div className="card-content">
                         <div className="content">
 
                             <button onClick={handleClick}
-                                className="button is-rounded is-info is-small is-fullwidth has-text-weight-bold" > Execute </button>
+                                className="button is-info is-small is-fullwidth has-text-weight-bold" > Execute </button>
                         </div>
                     </div>
 
