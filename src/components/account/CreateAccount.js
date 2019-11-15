04c7fb137ffd40099b21b9092cc16cb3e8aefd7f
johnsavadkuhi <physitime@gmail.com>
2019-11-15 22:56:29 +0330

    change a lot

--- src/components/account/CreateAccount.js
+++ src/components/account/CreateAccount.js
@@ -1,37 +1,93 @@
 import React, { useState } from 'react';
-import {Account } from '../../pweb3'; 
+import { Account } from '../../pweb3';
 
-function CreateAccount (props) {
+function CreateAccount(props) {
 
     const [isExec, setIsExec] = useState(false);
-    const [state , setState] = useState({address:'' , privateKey:''}); 
+    const [state, setState] = useState({ address: '', privateKey: '' });
 
-    const handleClick =async  e => {
+    const handleClick = e => {
         e.preventDefault();
-        
-        setIsExec(true); 
-        let {address , privateKey  } = Account.create() ; 
-        setState({address:address , privateKey:privateKey}) ; 
-        console.log(Account); 
+
+        setIsExec(true);
+        let { address, privateKey } = Account.create();
+        setState({ address: address, privateKey: privateKey });
 
     }
+
+    const handleCopyAddress = ()=>{
+
+        var  copyText = document.getElementById("account_address");
+        
+        /* Select the text field */
+        console.log(copyText)
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+        /* Copy the text inside the text field */
+
+        document.execCommand('copy');
+
+        
+    }
+    const handleCopyPrivateKey =()=>{
+
+        var  copyText = document.getElementById("privatekey");
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+    }
+
     const handleClose = () => {
         setIsExec(false);
+        setState({address:'' , privateKey:''})
     }
 
     return (
         <>
             {isExec ?
-                <div className="notification ">
-                    <button onClick={handleClose} className="delete"></button>
-
-                    <div>
-                        <div className="card-content">
-                            <pre> {JSON.stringify(state, null, 2)}</pre>
+                <>
+                    <article className="message  is-info">
+                        <div className="message-header">
+                            <p className="is-size-5 is-size-6-mobile">Account Info </p>
+                            <button className="delete" aria-label="delete" onClick={handleClose}></button>
                         </div>
+                        <div className="message-body">
 
-                    </div>
-                </div>
+                            <fieldset >
+                                <label >Address</label>
+
+                                <div className="field has-addons">
+
+                                    <p className="control is-expanded">
+                                        <input id="account_address" readOnly={true} className="input is-small" type="text" value={state.address} />
+                                    </p>
+                                    <div className="control">
+                                        <button  onClick={handleCopyAddress} className="button is-small">copy</button>
+                                    </div>
+
+                                </div>
+                                <label  >PrivateKey</label>
+                                <div className="field is-horizontal">
+                                
+                                    <div className="field-body">
+                                        <div className="field has-addons">
+                                            <div className="control is-expanded">
+                                                <textarea id="privatekey" readOnly={true} className="textarea is-small" value={state.privateKey} />
+                                            </div>
+                                            <div className="control">
+                                                <button  onClick={handleCopyPrivateKey} className="button is-small height_button">copy</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+                        </div>
+                    </article>
+
+                </>
+
                 :
                 <div className="card">
                     <header className="card-header">
