f9955781e8a600fc8ad603db9cb00396f33a36b0
johnsavadkuhi <physitime@gmail.com>
2019-11-16 03:17:13 +0330

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+function Footer(){
+
+    return(<>
+    <footer >
+  <div className="content has-text-centered">
+    <p>
+      <strong>PiScan.org</strong> by <a href="https://github.com/johnsavadkuhi">John Savadkuhi</a> for pchain network 
+    </p>
+  </div>
+</footer>
+    </>)
+}
+
+export default Footer ; 
