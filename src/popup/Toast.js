8cb6f3f50b142731659b25166276a0377ae90a51
johnsavadkuhi <physitime@gmail.com>
2019-11-16 06:20:31 +0330

    add new file

--- src/popup/Toast.js
+++ src/popup/Toast.js
@@ -11,3 +11,11 @@ export const Toast = Swal.mixin({
       toast.addEventListener('mouseleave', Swal.resumeTimer)
     }
   })
+
+  export const Warning = Swal.mixin({
+    toast:false ,
+    title : 'Keep Safe ',
+    text:"Keystore file includes encrypted privatekey keep safe it. ",
+    icon: 'warning',
+    showCancelButton:true 
+  })
