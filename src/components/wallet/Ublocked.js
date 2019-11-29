aa975193e461faa3ffdbff4244d50ad2638f1573
johnsavadkuhi <physitime@gmail.com>
2019-11-30 00:12:22 +0330

    add new component

--- src/components/wallet/Ublocked.js
+++ src/components/wallet/Ublocked.js
@@ -1,47 +1,75 @@
-import React from 'react';
-import Container from '../container';
+import React, { useState } from 'react';
+import Send from './Send';
+import Receive from './Receive';
 
 function Unlocked(props) {
 
-    const handleClickSend = ()=>{
-        
-        
+
+    const [selected, setSelected] = useState('wallet');
+
+    const handleClickSend = () => {
+
+        setSelected('send'); 
 
     }
+    
+    const handleClickReceive = ()=>{
+
+        setSelected('receive'); 
+    
+    }
+
+    const handleClose=()=>{
+        setSelected('wallet'); 
+    }
+    
     return (<>
+        
+        {
+            selected === "send" && <Send onClick={handleClose}/>
+        }
 
-        <div className="card">
-            <header className="card-header">
-                <p className="card-header-title">
-                    {props.name}
+        {
+          selected === "wallet" &&
+
+            <div className="card">
+                <header className="card-header">
+                    <p className="card-header-title">
+                        {props.name}
                     </p>
-                <span className="card-header-icon" >
-                <a onClick={handleClickSend}
-                 href="#" className="icon " style={{marginRight:'20px'}} >
-                        <i className="fa fa-send" ></i>
-                    </a>
+                    <span className="card-header-icon" >
+                        <a onClick={handleClickSend}
+                            href=" #" className="icon " style={{ marginRight: '20px' }} >
+                            <i className="fa fa-send" ></i>
+                        </a>
 
-                    <a href="#" className="icon" style={{marginRight:'10px'}}>
-                        <i className="fa fa-qrcode" ></i>
-                    </a>
-                    
-                </span>
-            </header>
-            
-            
-            <div className="card-content">
-                <div className="content">
-                    
+                        <a onClick={handleClickReceive}
+                         href=" #" className="icon" style={{ marginRight: '10px' }}>
+                            <i className="fa fa-qrcode" ></i>
+                        </a>
+
+                    </span>
+                </header>
+
+
+                <div className="card-content">
+                    <div className="content">
+
+                    </div>
                 </div>
+
+                <footer className="card-footer">
+                    <button className="is-fullwidth button  is-small ">Reload</button>
+                    <button className="button is-fullwidth is-small">Delete</button>
+                </footer>
             </div>
 
-            
-           
-            <footer className="card-footer">
-                <button  className="is-fullwidth button  is-small ">Reload</button>
-                <button className="button is-fullwidth is-small">Delete</button>
-            </footer>
-        </div>
+        }
+
+        {
+            selected==="receive" &&
+            <Receive onClick={handleClose}/>
+        }
 
     </>)
 }
