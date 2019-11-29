91f3aef364b697b9cffbebb28ca8b86416df0d43
johnsavadkuhi <physitime@gmail.com>
2019-11-30 00:13:25 +0330

    remove unsed code

--- src/components/wallet/index.js
+++ src/components/wallet/index.js
@@ -4,15 +4,12 @@ import Unlocked from './Ublocked';
 function Wallet() {
 
     return (
-       <div className="container">
-           <br/>
-       <Unlocked name="Wallet_1"/><br/>
-       <Unlocked name="Wallet_2"/><br/>
-       <Unlocked name="Wallet_3"/><br/>
-       <Unlocked name="Wallet_4"/><br/>
+        <div className="container">
+            <br />
+            <Unlocked name="Wallet_1" /><br />
 
-</div>
-   );
+        </div>
+    );
 }
 
 export default Wallet; 
