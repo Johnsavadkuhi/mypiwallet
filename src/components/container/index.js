967ac2771a75b665c16c62e226bab423cc59aa1b
johnsavadkuhi <physitime@gmail.com>
2019-11-29 09:28:11 +0330

    remove props.close

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -3,12 +3,11 @@ import React from 'react';
 function Container(props){
 
     return (<>
-    
-    
+
     <article className="message is-light">
                 <div className="message-header">
                     <code className=" has-text-danger is-size-5 is-size-6-mobile"> {props.header} </code> 
-                    {props.close}
+                    
                 </div>
                 <div className="message-body">
 
