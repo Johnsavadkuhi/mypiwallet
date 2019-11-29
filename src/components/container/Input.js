20dd583d2ebb3ab4eb3884d92be37e9d35003404
johnsavadkuhi <physitime@gmail.com>
2019-11-30 00:13:09 +0330

    add icon and type prop

--- src/components/container/Input.js
+++ src/components/container/Input.js
@@ -4,16 +4,20 @@ function Input(props){
 
     return( <div className="field">
     <div className="control has-icons-left">
+    
         <input value={props.value}
             onChange={props.onChange}
             id={props.id}
             readOnly={props.readOnly}
             className={props.className}
-             type="password" placeholder="Password" />
+            type={props.type} placeholder={props.placeholder} />
+       
         <span className="icon is-small is-left">
-            <i className="fa fa-lock" aria-hidden="true"></i>
+            <i className={`fa fa-${props.icon}`} aria-hidden="true"></i>
         </span>
+
     </div>
+
     <p id="helper" className="help">{props.helper}</p>
 
 </div>)
