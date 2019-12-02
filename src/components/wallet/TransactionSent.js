1b05cfca1c69527524075c37a04ee720758d9a73
johnsavadkuhi <physitime@gmail.com>
2019-12-03 00:07:46 +0330

    add Input component

--- src/components/wallet/TransactionSent.js
+++ src/components/wallet/TransactionSent.js
@@ -1,11 +1,12 @@
 import React from 'react'; 
+import Input from '../container/Input';
 
 function TransactionSent(props){
 
     return (<>
     <div className="box">
 
-        
+        <Input className="input is-small " value ={props.info.data.sendRawTransaction.transactionHash} />
 
     </div>
     </>); 
