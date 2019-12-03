9e8e8527a699498c5888a4b4b8b2445e43bc3397
johnsavadkuhi <physitime@gmail.com>
2019-12-04 01:07:05 +0330

    update

--- src/components/wallet/index.js
+++ src/components/wallet/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Imported  from './Imported';
+import Imported from './Imported';
 
 function Wallet() {
 
@@ -11,27 +11,33 @@ function Wallet() {
     }, [])
 
     return (
-       <>
-       {localStorage.length === 0 ?
-        <div className="container">
-            <br/>
-            <div className="has-text-centered is-size-3 ">
-            
-             No Wallet Imported Yet. 
+        <>
+            {localStorage.length === 0 ?
+                <div className="container">
+                    <br />
+                    <div className="has-text-centered is-size-3 ">
+
+                        No Wallet Imported Yet.
 
             </div>
-       </div>
-        : 
-       <div className="container">
-            {
+                </div>
+                :
+                <div>
+                    <div className="columns">
+                        <div className="column is-three-fifths is-offset-one-fifth">
 
-                keys.map((k) => (
-                    <Imported  name={k} key={k} />))
+                            {
+                                keys.map((k) => (
+                                    <Imported name={k} key={k} />
+                                ))
+                            }
 
+                        </div>
+                    </div>
+
+                </div>
             }
-        </div>
-}
-</>
+        </>
     );
 }
 
