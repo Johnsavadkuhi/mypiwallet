05e9dbe65a85dc5aa9e0ba665ce1983258e622b7
johnsavadkuhi <physitime@gmail.com>
2019-12-03 06:11:35 +0330

    update

--- src/pweb3/index.js
+++ src/pweb3/index.js
@@ -1,4 +1,4 @@
-const {   Accounts} = require('pweb3-eth-accounts');
+const { Accounts} = require('pweb3-eth-accounts');
 export const  Tx = require('pchainjs-tx');
 export const Account = new Accounts("http://localhost:6969/pchain") ; 
 
