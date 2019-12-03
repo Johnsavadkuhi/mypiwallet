e8b01d98cfec79acef348f671fd8d0257fb636e0
johnsavadkuhi <physitime@gmail.com>
2019-12-03 08:13:02 +0330

    update

--- src/components/wallet/index.js
+++ src/components/wallet/index.js
@@ -1,24 +1,22 @@
-import React , {useEffect , useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Unlocked from './Ublocked';
 
 function Wallet() {
 
-    const [keys , setKeys ]= useState([]); 
-    useEffect(()=>{
-       
-        setKeys(Object.keys(localStorage)); 
-        
+    const [keys, setKeys] = useState([]);
+    useEffect(() => {
+
+        setKeys(Object.keys(localStorage));
+
     }, [])
 
-   
-    
     return (
         <div className="container">
             {
-                
-                keys.map((k )=>(   
-                 <Unlocked name={k} key={k}/> ))
-            
+
+                keys.map((k) => (
+                    <Unlocked name={k} key={k} />))
+
             }
         </div>
     );
