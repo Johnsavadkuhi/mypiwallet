12c941fb1927e6e8e03721770178cc2c554efce2
johnsavadkuhi <physitime@gmail.com>
2019-12-03 06:12:34 +0330

    add new component

---
+++ src/components/wallet/PasswordForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Container from '../container';
+import Input from '../container/Input';
+
+function PasswordForm(props) {
+
+    return (<>
+            <Input className="is-small" value={props.password}
+                onChange={props.handleChangePassword}
+                helper={props.helper}
+                icon="lock"
+                placeholder="Password"
+                id="helper"
+            />
+            <div className="has-text-centered download_btn_margin">
+                <button onClick={props.handleSend}
+                    className="button is-info is-small is-fullwidth has-text-weight-bold" > Send </button>
+            </div>
+    </>)
+}
+
+export default PasswordForm; 
