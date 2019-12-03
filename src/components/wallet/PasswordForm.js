f4c36471f2e2c3a764574b61c2f95931bfbab3d9
johnsavadkuhi <physitime@gmail.com>
2019-12-03 06:19:18 +0330

    remove unused container

--- src/components/wallet/PasswordForm.js
+++ src/components/wallet/PasswordForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Container from '../container';
 import Input from '../container/Input';
 
 function PasswordForm(props) {
@@ -13,7 +12,7 @@ function PasswordForm(props) {
                 id="helper"
             />
             <div className="has-text-centered download_btn_margin">
-                <button onClick={props.handleSend}
+                <button onClick={props.onClick}
                     className="button is-info is-small is-fullwidth has-text-weight-bold" > Send </button>
             </div>
     </>)
