2fd2d532e7c6e04ab30e269be3438eb665dfa06c
johnsavadkuhi <physitime@gmail.com>
2019-12-04 11:55:21 +0330

    update

--- src/components/wallet/SignedTransaction.js
+++ src/components/wallet/SignedTransaction.js
@@ -5,10 +5,10 @@ function SignedTransaction(props) {
 
     return (<>
            
-           <Textarea defaultValue ={props.defaultValue}/>
+           <Textarea defaultValue ={props.defaultValue} className="has-text-grey"/>
             <div className="has-text-centered download_btn_margin">
                 <button onClick={props.onClick}   
-                    className="button is-info is-small is-fullwidth has-text-weight-bold" > send </button>
+                    className={`button is-info is-small is-fullwidth has-text-weight-bold ${props.bClassName}` }> send </button>
             </div>
     </>)
 }
