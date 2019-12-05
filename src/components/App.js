8f96731bbb620c71f558fe277baa07dcff250419
johnsavadkuhi <physitime@gmail.com>
2019-12-05 13:19:15 +0330

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import '../style/main.scss'
 import Navigation from './navigation';
-import Blocks from './Blocks'; 
-import Contracts from './Contracts'; 
-import Utilities from './Utilities'; 
 import Account from './account/Account';
-import Transactions from './transaction/Transactions';
 import Wallet from './wallet'; 
 
 import {
@@ -29,13 +25,9 @@ function App() {
           
           <Route path="/wallet" component={Wallet}/>
             
-          <Route path="/transactions" component={Transactions}/>
             
-          <Route path="/contracts" component={Contracts}/>
-          
-          <Route path="/utilities" component={Utilities}/>
+         
             
-          <Route path="/blocks" component={Blocks}/>
               
         </Switch>
 
