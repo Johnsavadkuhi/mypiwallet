62a9e0403fb3d4de5d6769e20ed56a8977e5e860
johnsavadkuhi <physitime@gmail.com>
2019-12-06 02:24:25 +0330

    add Home component

---
+++ src/components/home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+
+
+function Home(props){
+    return (<>
+    
+    <div className="container">
+
+        <p className="is-size-3">
+            MyPiWallet 
+        </p>
+
+    </div>
+    
+    </>)
+}
+
+export default Home ; 
