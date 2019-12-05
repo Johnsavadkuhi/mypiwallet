3fb27fc612a93bc9e3fc260bb78c8154eb1c3afd
johnsavadkuhi <physitime@gmail.com>
2019-12-05 10:05:17 +0330

    add new component

---
+++ src/components/tabs/Account.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+
+
+function Account(props){
+
+    return(<>
+    <div class="tabs is-right">
+  <ul>
+    <li class="is-active"><a>Pictures</a></li>
+    <li><a>Music</a></li>
+    <li><a>Videos</a></li>
+    <li><a>Documents</a></li>
+  </ul>
+</div>
+    </>); 
+}
+
+export default Account ;  
