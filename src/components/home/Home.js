9a4660cffef6dd8728f081098a83837b2f99f1ce
johnsavadkuhi <physitime@gmail.com>
2019-12-07 19:57:16 +0330

    update

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import { AnimateOnChange } from 'react-animation'
+// import { AnimateOnChange } from 'react-animation'
+import { animations, easings } from 'react-animation'
+
+const style = {
+    animation: `pop-in ${easings.easeOutExpo} 500ms forwards`
+}
+const style1 = {
+    animation: `pop-in ${easings.easeOutExpo} 500ms forwards`
+}
+const style2 = {
+    animation: animations.popIn
+}
 
 
 
@@ -9,29 +20,73 @@ function Home() {
     return (<>
 
         <div className="container-fluid" style={{ margin: "15px" }}>
-                       
-            <section className="hero is-medium  is-bold " >
+
+            <section className="hero is-medium  is-bold "  >
                 <div className="hero-body">
 
-                    <div className="container">
-                        <h1 className="title is-size-1">
-                            <AnimateOnChange
-                                animationIn="bounceIn"
-                                animationOut="bounceOut"
-                                durationOut={5000}>
+                    <div className="container" style={style2}>
+                        <h1 className="title is-size-1 is-size-3-mobile " style={style}>
 
-                                MyPiWallet.org
-                                
-                            </AnimateOnChange>
-                       
+                            MyPiWallet.org
                         </h1>
 
-                        <h2 className="subtitle"  >
+                        <p className=" subtitle is-size-5 is-size-6-mobile" style={style1}>
                             Pchain Wallet web base!
-                        </h2>
+                        </p>
+                        <br/>
+                            <hr/>
+                        <div className="contnet" >
 
-                        <div className="contnet">
+                            <div className="columns">
+                                
+                                <div className="column is-4" >
+                                    <div className="notification has-background-white has-text-justified">
+                                        <p className="is-size-6 box " style={style2}>
 
+                                            <strong>MyPiWallet.org</strong> provides a way to 
+                                            <a style={{textDecoration:'none'}} className="has-text-info" href="/account"><strong> Create Wallet</strong> </a>
+                                            and Import Wallet,
+                                            after creation you can import it for importing you should Enter a name and password
+                                             and privateKey at the End.in 
+                                             <a style={{textDecoration:'none'}} className="has-text-info" href="/wallet"><strong> Wallet </strong> </a> 
+                                             sending you can send pi to another Wallet. and also you can see the 
+                                             <a style={{textDecoration:'none'}} className="has-text-info" href="/account"><strong> Wallet Balance</strong> </a>.
+                                            
+                                            
+                                        </p>
+                                    </div>
+
+                                </div>
+
+
+                                <div className="column is-4">
+                                    <div className="notification has-background-white has-text-justified is-danger ">
+                                        <p className="is-size-6 box  ">
+                                            <strong className="has-text-danger">PrivateKey : </strong>
+                                             pchain privateKey starts with 0x and hash 64 characters without 0x 
+                                             and is used to sign a transaction 
+                                             when you send a transaction to pchain network at the first time, you should sign 
+                                             the transaction with privateKey and then send the transaction to pchain network. 
+                                             
+                                            
+                                        </p>
+                                    </div>
+
+                                </div>
+
+
+
+                                <div className="column is-4">
+                                    <div className="notification has-background-white has-text-justified is-danger ">
+                                        <p className="is-size-6 box  ">
+                                            <strong className="has-text-danger">Password : </strong> 
+                                            is used to encrypt the wallet privateKey, you should store password in a safe place 
+                                            because MyPiWallet.org can not recover your password. 
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
