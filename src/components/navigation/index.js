06c9b576080c29830ae6ae0b0fc28a8669691c08
johnsavadkuhi <physitime@gmail.com>
2019-12-07 10:36:05 +0330

    update

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,67 +1,78 @@
-import React  from 'react';
+import React from 'react';
 import logo from '../../images/miw.png';
 import { NavLink } from 'react-router-dom';
 
 
 function Navigation() {
 
- const handlClick =()=>{
-   
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-   
+  const handlClick = () => {
+
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
     if ($navbarBurgers.length > 0) {
-      
+
       // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
+      $navbarBurgers.forEach(el => {
 
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('overlapMenu');
-  
-      }); 
-    }  
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('overlapMenu');
+
+      });
+    }
   }
 
-  const handlMenuItemClick = () =>{
-    
+  const handlMenuItemClick = () => {
+
   }
- 
+
 
   return (<>
 
     <nav className="navbar is-light" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <NavLink className="navbar-item" to="/" >
-          <img src={logo} width="112" height="90"  alt="logo" />
+          <img src={logo} width="112" height="90" alt="logo" />
         </NavLink>
 
-        <span role="button"  className="navbar-burger burger  " aria-label="menu"
-        aria-expanded="false" data-target="navbarBasicExample" onClick={handlClick}>
+        <span role="button" className="navbar-burger burger  " aria-label="menu"
+          aria-expanded="false" data-target="navbarBasicExample" onClick={handlClick}>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </span>
       </div>
 
-      <div id="navbarBasicExample" aria-expanded="false" className="navbar-menu "  role="menu" onClick={handlMenuItemClick} >
+      <div id="navbarBasicExample" aria-expanded="false" className="navbar-menu " role="menu" onClick={handlMenuItemClick} >
         <div className="navbar-start"  >
           <NavLink to="/" className="navbar-item">
             Home
             </NavLink>
-        <NavLink to="/account" className="navbar-item " >
-          Account
+          <NavLink to="/account" className="navbar-item " >
+            Account
         </NavLink>
 
-        <NavLink to="/wallet" className="navbar-item">
-          Wallet
+          <NavLink to="/wallet" className="navbar-item">
+            Wallet
         </NavLink>
-         
+
         </div>
 
+        <div className="navbar-end">
+          <div className="navbar-item">
+              
+              <div class="navbar-item has-dropdown">
+                <span href=" " className="navbar-link">
+                  English
+              </span>
+              =  
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
 
