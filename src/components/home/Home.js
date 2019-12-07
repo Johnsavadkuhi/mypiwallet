51b058ebcc8b7b7456621900d9d02ab3b7b4ef67
johnsavadkuhi <physitime@gmail.com>
2019-12-07 18:18:46 +0330

    update

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,26 +1,37 @@
 import React from 'react';
-// import logo from '../../images/t.png';
+import { AnimateOnChange } from 'react-animation'
 
 
-function Home(props) {
+
+function Home() {
+
+
     return (<>
 
         <div className="container-fluid" style={{ margin: "15px" }}>
-
-            <section className="hero is-medium  is-bold">
+                       
+            <section className="hero is-medium  is-bold " >
                 <div className="hero-body">
-                   
+
                     <div className="container">
                         <h1 className="title is-size-1">
-                            MyPiWallet.org
+                            <AnimateOnChange
+                                animationIn="bounceIn"
+                                animationOut="bounceOut"
+                                durationOut={5000}>
+
+                                MyPiWallet.org
+                                
+                            </AnimateOnChange>
+                       
                         </h1>
 
-                        <h2 className="subtitle">
+                        <h2 className="subtitle"  >
                             Pchain Wallet web base!
                         </h2>
 
                         <div className="contnet">
-                            
+
                         </div>
                     </div>
                 </div>
@@ -31,4 +42,5 @@ function Home(props) {
     </>)
 }
 
-export default Home; 
+export default Home;
+
